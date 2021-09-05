810492d41b765b919b13dc877986e82c8ede2faa
Toktar <ufazak@gmail.com>
2021-09-05 23:27:53 +0600

    Определяем отправляемый тип контента как html

--- src/main/java/kz/f12/school/HelloWorldServlet.java
+++ src/main/java/kz/f12/school/HelloWorldServlet.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.write("<h1>Hello World from Servlet</h1>");
     }
--- src/main/java/kz/f12/school/LoginPageServlet.java
+++ src/main/java/kz/f12/school/LoginPageServlet.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 public class LoginPageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.write("<h1>Login page Servlet</h1>");
     }
